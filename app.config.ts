09c3aab3f5c786881b960a5e61f3efadd066f313
Luis Fonseca <luis.fonsecachinchilla@ucr.ac.cr>
2026-05-13 21:55:45 -0600

    chore: migrate app.json to app.config.ts
    Convert static app.json to TypeScript config so EXPO_PUBLIC_GOOGLE_MAPS_PLATFORM_API_KEY
    can be injected at config-resolution time. Also adds:
    - iOS NSLocationWhenInUseUsageDescription and LSApplicationQueriesSchemes
    - Android ACCESS_FINE/COARSE_LOCATION permissions
    - Android/iOS Google Maps API key under android.config.googleMaps and ios.config

---
+++ app.config.ts
@@ -0,0 +1,89 @@
+import type { ExpoConfig } from "expo/config";
+
+const googleMapsApiKey =
+  process.env.EXPO_PUBLIC_GOOGLE_MAPS_PLATFORM_API_KEY ?? "";
+
+const config: ExpoConfig = {
+  name: "CI-0161_appmoviles_2026a",
+  slug: "CI-0161_appmoviles_2026a",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "ci0161appmoviles2026a",
+  userInterfaceStyle: "automatic",
+
+  ios: {
+    icon: "./assets/expo.icon",
+    bundleIdentifier: "com.ci0161.appmoviles2026a",
+    config: {
+      googleMapsApiKey,
+    },
+    infoPlist: {
+      NSLocationWhenInUseUsageDescription:
+        "We use your location to show nearby beaches, parks, and places.",
+      LSApplicationQueriesSchemes: ["comgooglemaps", "googlechromes"],
+    },
+  },
+
+  android: {
+    adaptiveIcon: {
+      backgroundColor: "#E6F4FE",
+      foregroundImage: "./assets/images/android-icon-foreground.png",
+      backgroundImage: "./assets/images/android-icon-background.png",
+      monochromeImage: "./assets/images/android-icon-monochrome.png",
+    },
+    predictiveBackGestureEnabled: false,
+    package: "com.ci0161.appmoviles2026a",
+    permissions: [
+      "android.permission.ACCESS_COARSE_LOCATION",
+      "android.permission.ACCESS_FINE_LOCATION",
+    ],
+    config: {
+      googleMaps: {
+        apiKey: googleMapsApiKey,
+      },
+    },
+    intentFilters: [
+      {
+        action: "VIEW",
+        data: [
+          { scheme: "ci0161appmoviles2026a" },
+          {
+            scheme:
+              "com.googleusercontent.apps.1047179573348-6ldp2q7q9589qfcrf0t579c8ien92617",
+          },
+        ],
+        category: ["BROWSABLE", "DEFAULT"],
+      },
+    ],
+  },
+
+  web: {
+    output: "static",
+    favicon: "./assets/images/favicon.png",
+  },
+
+  plugins: [
+    "expo-router",
+    [
+      "expo-splash-screen",
+      {
+        backgroundColor: "#208AEF",
+        android: {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 76,
+        },
+      },
+    ],
+    "expo-secure-store",
+    "@react-native-google-signin/google-signin",
+    "expo-web-browser",
+  ],
+
+  experiments: {
+    typedRoutes: true,
+    reactCompiler: true,
+  },
+};
+
+export default config;
